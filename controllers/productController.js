a73c1a60a2dc1651e56cf1a6d45fd1d1be2e63df
GITvoren <tejuco.voren@gmail.com>
2023-06-13 16:24:33 +0800

    added new functions

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,35 @@
 const Product = require('../models/Product.js')
 
 
-
+// Get All Products
 const getProducts = (req, res) => {
-     Product.find({})
+     Product.find({})/* .select('name description -_id') */
      .then(products => res.json(products))
-     .catch(err => res.sendStatus(500));
+     .catch(() => res.sendStatus(500));
 }
 
+// Get All Active Products
+const getActiveProducts = (req, res) => {
+     Product.find({isActive: true})
+     .then(activeProducts => res.json(activeProducts))
+     .catch(() => res.sendStatus(500));
+}
+
+// Get Single Product
+const getProduct = (req, res) => {
+     Product.findById(req.params.id)
+     .then(product => {
+          if(!product)
+          return res.status(400).json("Product not found");
+
+          res.json(product);
+     })
+     .catch(err => res.status(400).json(err.message));
+}
+
+// Add Product
 const addProduct = (req, res) => {
-     product = new Product({
+     const product = new Product({
           name: req.body.name,
           description: req.body.description,
           price: req.body.price
@@ -20,7 +40,7 @@ const addProduct = (req, res) => {
      .catch(err => res.status(400).json(err.message));
 }
 
-
+// Delete Product
 const deleteProduct = (req, res) => {
      Product.findById(req.params.id)
      .then(product => {
@@ -30,9 +50,10 @@ const deleteProduct = (req, res) => {
           product.deleteOne();
           res.json("Successfully Deleted Product");
      })
-     .catch(err => res.status(400).json(err.message))
+     .catch(err => res.status(400).json(err.message));
 }
 
+// Update Product
 const updateProduct = (req, res) => {
     const updatedProduct = {
       name: req.body.name,
@@ -43,17 +64,45 @@ const updateProduct = (req, res) => {
     Product.findByIdAndUpdate(req.params.id, updatedProduct, {new: true})
     .then(result => {
        if(!result)
-       return res.status(400).json("Product not found")
+       return res.status(400).json("Product not found");
 
        res.json(result);
     })
     .catch(err => res.status(400).json(err.message));
 }
 
+// Archive Product (isActive -> false)
+const archiveProduct = (req, res) => {
+     Product.findById(req.params.id)
+     .then(product => {
+          product.isActive = false
+          product.save()
+          .then(updatedProduct => res.json(updatedProduct))
+          .catch(() => res.sendStatus(500));
+     })
+     .catch(err => res.status(400).json(err.message));
+}
+
+// Activate Product (isActive -> true)
+const activateProduct = (req, res) => {
+     Product.findById(req.params.id)
+     .then(product => {
+          product.isActive = true
+          product.save()
+          .then(updatedProduct => res.json(updatedProduct))
+          .catch(() => res.sendStatus(500));
+     })
+     .catch(err => res.status(400).json(err.message));
+}
+
 
 module.exports = {
      getProducts,
+     getActiveProducts,
+     getProduct,
      addProduct,
      deleteProduct,
-     updateProduct
+     updateProduct,
+     archiveProduct,
+     activateProduct
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,20 +2,39 @@ const router = require('express').Router();
 const productController = require('../controllers/productController.js');
 const {
      getProducts,
+     getActiveProducts,
+     getProduct,
      addProduct,
      deleteProduct,
-     updateProduct
+     updateProduct,
+     archiveProduct,
+     activateProduct
 
 } = productController;
 
-
+// Get All Products
 router.get('/', getProducts);
 
+// Get All Active Products
+router.get('/active', getActiveProducts);
+
+// Get Single Product
+router.get('/:id', getProduct);
+
+// Add Product
 router.post('/', addProduct);
 
+// Delete Product
 router.delete('/:id', deleteProduct);
 
+// Update Product
 router.put('/:id', updateProduct);
+
+// Archive Product (isActive -> false)
+router.patch('/:id/archive', archiveProduct);
+
+// Activate Product (isActive -> true)
+router.patch('/:id/activate', activateProduct);
 
 
 module.exports = router;
